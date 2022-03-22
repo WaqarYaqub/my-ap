d5edfade9f903f42b9b60a4c59589bd1abf46dbe
Waqar-Yaqub <waqar.yaqub@pikessoft.com>
2022-03-22 14:22:16 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='waqar.jpg' className="App-logo" alt="logo" />
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+        <h1>Waqar Yaqub</h1>
+        <h5>waqar.yaqub.pk@gmail.com</h5>
+        <h3>Software Engineer at Pikessoft</h3>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
